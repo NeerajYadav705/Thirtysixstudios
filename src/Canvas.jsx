62c121dc75a719677784c40fc9733656b659f8b3
Neeraj Yadav <neeraj95578@gmail.com>
2024-11-14 14:52:28 +0530

    first commit

---
+++ src/Canvas.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useRef, useState } from "react";
+import image from "./image";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+function Canvas({ details }) {
+  const { startIndex, numImages, duration, size, top, left, zIndex } = details;
+
+  const [index, setIndex] = useState({ value: startIndex });
+  const canvasRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.to(index, {
+      value: startIndex + numImages - 1,
+      duration: duration,
+      repeat: -1,
+      ease: "linear",
+      onUpdate: () => {
+        setIndex({ value: Math.round(index.value) });
+      },
+    });
+    gsap.from(canvasRef.current, {
+      opacity: 0,
+      duration: 0.8,
+      ease: "power2.inOut",
+    });
+  });
+
+  useEffect(() => {
+    const scale = window.devicePixelRatio;
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    img.src = image[index.value];
+    img.onload = () => {
+      canvas.width = canvas.offsetWidth * scale;
+      canvas.height = canvas.offsetHeight * scale;
+      canvas.style.width = canvas.offsetWidth + "px";
+      canvas.style.height = canvas.offsetHeight + "px";
+
+      ctx.scale(scale, scale);
+      ctx.drawImage(img, 0, 0, canvas.offsetWidth, canvas.offsetHeight);
+    };
+  }, [index]);
+
+  return (
+    <canvas
+      data-scroll
+      data-scroll-speed={Math.random().toFixed(1)}
+      ref={canvasRef}
+      className="absolute"
+      id="canvas"
+      style={{
+        width: `${size * 1.3}px`,
+        height: `${size * 1.3}px`,
+        top: `${top}%`, // Directly use top from details
+        left: `${left}%`, // Directly use left from details
+        zIndex: `${zIndex}`,
+      }}
+    ></canvas>
+  );
+}
+
+export default Canvas;
